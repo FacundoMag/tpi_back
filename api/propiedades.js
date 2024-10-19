bce9f4813474d9bdfcd50af26f21fc6397feaa84
luclas123 <lc2090365@gmail.com>
2024-10-18 21:52:09 -0300

    newpropiedades actualizado

--- api/propiedades.js
+++ api/propiedades.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const conexion = require('../db/conexion');
+const { hashPass, verificarPass, generarToken, verificarToken } = require('@damianegreco/hashpass');
+
+const TOKEN_SECRET = "EQUIPO_GOAT"
 
 // Obtener todas las propiedades
 router.get('/', (req, res) => {
@@ -30,12 +33,29 @@ router.get('/:id', (req, res) => {
 
 // Crear una nueva propiedad
 router.post('/newpropiedad', (req, res) => {
-    const { usuario_id, direccion, ciudad_id, num_habitaciones, num_banos, capacidad, tamano_m2, precio_renta, tipo_id, estado, descripcion } = req.body;
+    const token = req.headers.authorization;
+    console.log(token)
+    if (!token) {
+        console.err('no tienes token');
+        res.status(403).json({
+            status: 'error',
+            error: "no tienes token"
+        })
+    }
+
+    const verificacionToken = verificarToken(token, TOKEN_SECRET);
+
+    req.usuario_id = verificacionToken?.data?.usuario_id;
+
+
+    const {nombre, direccion, ciudad_id, num_habitaciones, num_banos, capacidad, tamano_m2, precio_renta, tipo_id, estado_id, descripcion } = req.body;
+    const usuario_id = req.usuario_id;
     const sql = `INSERT INTO propiedades 
-                 (usuario_id, direccion, ciudad_id, num_habitaciones, num_banos, capacidad, tamano_m2, precio_renta, tipo_id, estado, descripcion) 
-                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-    conexion.query(sql, [usuario_id, direccion, ciudad_id, num_habitaciones, num_banos, capacidad, tamano_m2, precio_renta, tipo_id, estado, descripcion], (err, result) => {
+                 (registra_usuario_id, nombre, direccion, ciudad_id, num_habitaciones, num_banos, capacidad, tamano_m2, precio_renta, tipo_id, estado_id, descripcion) 
+                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    conexion.query(sql, [usuario_id, nombre, direccion, ciudad_id, num_habitaciones, num_banos, capacidad, tamano_m2, precio_renta, tipo_id, estado_id, descripcion], (err, result) => {
         if (err) {
+            console.log(err)
             return res.status(500).json({ error: err.message });
         }
         res.status(201).json({ message: 'Propiedad creada con éxito', propiedadId: result.insertId });
@@ -50,6 +70,7 @@ router.put('/:id', (req, res) => {
                  WHERE id = ?`;
     conexion.query(sql, [usuario_id, direccion, ciudad_id, num_habitaciones, num_banos, capacidad, tamano_m2, precio_renta, tipo_id, estado, descripcion, id], (err, result) => {
         if (err) {
+            console.log(err)
             return res.status(500).json({ error: err.message });
         }
         if (result.affectedRows === 0) {
