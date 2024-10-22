4295ef0a867a5f16d380981dbca4866f93b0c9df
FacundoMag <facundo.maggeri.0608@gmail.com>
2024-10-22 19:26:09 -0300

    logica de las reservaciones terminada

--- api/reservacion.js
+++ api/reservacion.js
@@ -28,6 +28,26 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// Obtener fechas ocupadas para una propiedad específica
+router.get('/fechas-ocupadas/:propiedadId', (req, res) => {
+    const { propiedadId } = req.params;
+    console.log(`Propiedad ID recibido: ${propiedadId}`); // Para verificar el ID que llega en la petición
+    
+    const sql = 'SELECT fecha_inicio AS inicio, fecha_fin AS fin FROM reservaciones WHERE propiedad_id = ?';
+    conexion.query(sql, [propiedadId], (err, results) => {
+        if (err) {
+            console.log('Error en la consulta:', err); // Para verificar si hay algún error en la consulta
+            return res.status(500).json({ error: err.message });
+        }
+        console.log('Resultados:', results); // Para verificar los resultados que devuelve la consulta
+        if (results.length === 0) {
+            return res.status(404).json({ message: 'No hay reservas para esta propiedad.' });
+        }
+        res.json(results);
+    });
+});
+
+
 // Crear una nueva reservación
 router.post('/', (req, res) => {
     const { propiedad_id, fecha_inicio, fecha_fin, fecha_reserva, monto_total } = req.body;
